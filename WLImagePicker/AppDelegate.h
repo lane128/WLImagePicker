50bed7346bf5224f760d82cc1d84fba70fc8b8bf
Lane <wang.lei01@whaley.cn>
2017-12-18 15:48:30 +0800

    init project

---
+++ WLImagePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WLImagePicker
+//
+//  Created by Lane on 2017/12/18.
+//  Copyright © 2017年 Lane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WLImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WLImagePicker
+//
+//  Created by Lane on 2017/12/18.
+//  Copyright © 2017年 Lane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
